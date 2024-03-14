b8ddb3f8713dc0f2581c3f703cec25acbbfad093
Wabu-K <KobayashiKenta@lifull.com>
2024-03-15 00:57:40 +0900

    chore: hayato94087/nextjs-mdx-sampleの内容を取り込み

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,37 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import nextMDX from "@next/mdx";
+import remarkGfm from "remark-gfm";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import rehypePrism from "@mapbox/rehype-prism";
+import rehypeSlug from "rehype-slug";
+import remarkToc from "remark-toc";
+import remarkBreaks from "remark-breaks";
 
-export default nextConfig;
+const withMDX = nextMDX({
+  extensions: /\.mdx?$/,
+  options: {
+    // If you use remark-gfm, you'll need to use next.config.mjs
+    // as the package is ESM only
+    // https://github.com/remarkjs/remark-gfm#install
+    remarkPlugins: [
+      remarkGfm,
+      remarkMath,
+      [remarkToc, { maxDepth: 3, heading: "目次" }],
+      remarkBreaks,
+    ],
+    rehypePlugins: [rehypeKatex, rehypePrism, rehypeSlug],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Configure pageExtensions to include md and mdx
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+  // Optionally, add any other Next.js config below
+  reactStrictMode: true,
+};
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig);
